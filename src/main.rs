e8c3aac92c443d42c01acaa3861b1ab7ab731619
mdvillagra <mdvillagra@gmail.com>
2023-09-09 13:31:34 -0400

    added linear_layer function.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
 extern crate poseidon_lib;
 
-
-use ark_ff::{Field, PrimeField, FpConfig, BigInteger};
 use ark_bls12_381::{Fq, FrConfig};
-use ark_std::{One, Zero, UniformRand};
 use ark_ff::BigInt as arkBigInt;
+use ark_ff::{BigInteger, Field, FpConfig, PrimeField};
+use ark_std::{One, UniformRand, Zero};
 
 use num_bigint::BigInt;
 use num_traits::Num;
 
 fn main() {
-  
-  let mut constants = poseidon_lib::read_constants("constants2.txt");
+    let mut constants = poseidon_lib::read_constants("constants2.txt");
 
-  let mut state: Vec<Fq> = Vec::new();
-  poseidon_lib::init_state(&mut state, 2);
+    let mut state: Vec<Fq> = Vec::new();
+    poseidon_lib::init_state(&mut state, 2);
 
-  poseidon_lib::ark(&mut state, &mut constants);
+    poseidon_lib::ark(&mut state, &mut constants);
 
-  println!("{}",state[0]);
+    println!("{}", state[0]);
 
-  poseidon_lib::sbox(&mut state, constants, true);
+    poseidon_lib::sbox(&mut state, constants, true);
 
-  println!("{}",state[0]);
+    println!("{}", state[0]);
 }
-
