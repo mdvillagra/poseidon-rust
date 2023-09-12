42c52fdadb108a9947e8824b8fda9ab8e6e02964
mdvillagra <mdvillagra@gmail.com>
2023-09-12 16:49:42 -0400

    separated the constant reading functions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use ark_bls12_381::Fq as Fbls12_381;
 use ark_std::UniformRand;
 
 fn main() {
-    let constants = poseidon_lib::read_constants_bls12381_n255_t5_alpha5_M128_RF8_RP56();
+    let constants =
+        poseidon_lib::read_constants::read_constants_bls12381_n255_t5_alpha5_M128_RF8_RP56();
     let mut rng = ark_std::test_rng();
     let mut input: Vec<Fbls12_381> = Vec::new();
 
@@ -13,7 +14,7 @@ fn main() {
 
     let output = poseidon_lib::hash(&input, &constants, 3, 3);
     println!("length is {}", output.len());
-    for c in output.into_iter().map(|elem| elem.to_string()){
+    for c in output.into_iter().map(|elem| elem.to_string()) {
         println!("{c}");
     }
 }
